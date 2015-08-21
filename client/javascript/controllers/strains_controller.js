e92984481fb896223eec248cff18704953f07f4e
Vikash Loomba <loomba.vikash@gmail.com>
2015-08-21 11:07:44 -0700

    Search feature implemented on the strains page

--- client/javascript/controllers/strains_controller.js
+++ client/javascript/controllers/strains_controller.js
@@ -3,6 +3,12 @@ myApp.controller('StrainsController', function ($scope, StrainFactory) {
 	StrainFactory.getStrains(function (response){
 		$scope.strains = response;
 		console.log('logging the strains: ', $scope.strains);
+		$scope.list = $scope.$parent.strains
+		  $scope.config = {
+		    itemsPerPage: 5,
+		    fillLastPage: true
+		  }
+
 	});
 
 
