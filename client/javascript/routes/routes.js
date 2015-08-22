301cca293068a397f5d0fd0bdee50918e5919d84
thaijaso <jasonwearsprada@gmail.com>
2015-08-21 21:02:10 -0700

    fixed description on reserve .html

--- client/javascript/routes/routes.js
+++ client/javascript/routes/routes.js
@@ -1,4 +1,4 @@
-var myApp = angular.module('myApp', ['ngRoute', 'uiGmapgoogle-maps', 'angular-loading-bar']);
+var myApp = angular.module('myApp', ['ngRoute', 'uiGmapgoogle-maps', 'angular-loading-bar', 'ngSanitize']);
 
 myApp.config(function ($routeProvider, uiGmapGoogleMapApiProvider) {
 	$routeProvider
