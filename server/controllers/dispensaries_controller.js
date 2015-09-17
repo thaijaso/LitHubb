67dc0cdcdbdbe900a19e2f3905e9745ff61e8580
Judd Deaver <judddeaver@gmail.com>
2015-09-17 15:57:07 -0700

    converge RESTful route

--- server/controllers/dispensaries_controller.js
+++ server/controllers/dispensaries_controller.js
@@ -11,9 +11,49 @@ connection.connect();
 module.exports = (function() {
 	return {
 		get: function(req, res) {
-			connection.query("SELECT * FROM greencommerce.officialvendors JOIN vendors ON vendors.address = officialvendors.address WHERE officialvendors.PrivilegeStatus = 'Active (Issued)'", function(error, results) {
+			const lngDistancePerDegreeAtEquator = 69.172;
+			const latDistancePerDegreeAtEquator = 69.08558338297732;
+
+			var maxRadius;
+			var fromLat;
+			var fromLng;
+			var sqlQuery;
+
+			if (typeof req.query.radius != "undefined"){
+				maxRadius = req.query.radius;
+			} else {
+				maxRadius = 5;
+			}
+			if (typeof req.query.lat != "undefined")
+			{
+				if (typeof req.query.lng != "undefined")
+				{
+					fromLat = req.query.lat;
+					fromLng = req.query.lng;
+					sqlQuery = 'SELECT *, ' +
+						'( 3959 * acos( cos( ' +
+							'radians(' + fromLat + ' ) ) * ' +
+							'cos( radians( vendors.lat ) ) * ' +
+							'cos( radians(vendors.lng) - ' +
+							'radians(' + fromLng + ')) + ' +
+							'sin(radians(' + fromLat + ')) * ' +
+							'sin( radians(vendors.lat)))) AS distance ' +
+						'FROM vendors ' +
+						'WHERE TRIM(PrivilegeStatus) = "ACTIVE (ISSUED)" ' +
+						'HAVING distance < ' + maxRadius + ' ' +
+						'ORDER BY distance';
+				} else {
+					error += "Must supply both longitude & latitude in the form lithubb.com/dispensaries?lat=47.609812&lng=-122.196568";
+				}
+			} else if (typeof req.query.lng != "undefined")
+			{
+				error += "Must supply both longitude & latitude in the form lithubb.com/dispensaries?lat=47.609812&lng=-122.196568";
+			} else {
+				sqlQuery = 'SELECT * FROM vendors WHERE TRIM(PrivilegeStatus) = "ACTIVE (ISSUED)"';
+			}
+			connection.query(sqlQuery, function(error, results) {
 				res.json(results);
 			});
-		}	
+		}
 	}
 })();
