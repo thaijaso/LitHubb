460b2558c27c252f94087aef67dc8b2cf6829e98
thaijaso <jasonwearsprada@gmail.com>
2015-09-25 16:21:13 -0700

    changed port

--- server.js
+++ server.js
@@ -5,13 +5,16 @@ var mysql = require('mysql');
 var session = require('express-session');
 
 var app = express();
+app.set('port', process.env.PORT || 8081);
+
 app.use(bodyParser.urlencoded());
 app.use(session({secret: '123'}));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, './client')));
 
+
 require('./config/routes.js')(app);
 
-app.listen(7000, function() {
-  	console.log('listening on port: 7000');
+app.listen(app.get('port'), function() {
+  	console.log('listening on port: ', app.get('port'));
 });
